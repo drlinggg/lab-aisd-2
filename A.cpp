299f3467d2a021c126df9d35902d20a87337afad
Andrey Banach <124909828+drlinggg@users.noreply.github.com>
2023-12-23 02:53:28 +0300

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
 
-void quicksort(int* array, int* second_array, int size) {
-    int index_left_side_element = 0;
-    int index_right_side_element = size - 1;
-
-    if (size < 2) {
-        return;
-    }
-
-    int middle = array[size/2];
-
-    while (index_left_side_element <= index_right_side_element) {#include <iostream>
-
 int main() {
     int number_of_tapoks;
     std::cin >> number_of_tapoks;
